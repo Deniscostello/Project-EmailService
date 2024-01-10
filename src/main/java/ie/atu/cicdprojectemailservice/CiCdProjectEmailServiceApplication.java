46d47e0531fb864ce87fd02c4e49d8f13664bc0d
Denis Costello <G00396309@atu.ie>
2024-01-10 17:02:23 +0000

    First commit

---
+++ src/main/java/ie/atu/cicdprojectemailservice/CiCdProjectEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdprojectemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdProjectEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdProjectEmailServiceApplication.class, args);
+    }
+
+}
