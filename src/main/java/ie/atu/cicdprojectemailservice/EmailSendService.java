6776fe64d9eec1eec0a27cdacb165174cc070cd5
Denis Costello <G00396309@atu.ie>
2024-01-10 17:13:41 +0000

    Added user class

--- src/main/java/ie/atu/cicdprojectemailservice/EmailSendService.java
+++ src/main/java/ie/atu/cicdprojectemailservice/EmailSendService.java
@@ -13,7 +13,7 @@ public class EmailSendService {
 
     public void sendEmail(String toEmail, String subject, String body){
         SimpleMailMessage message= new SimpleMailMessage();
-        message.setFrom("deniscostello309@gmail.com");
+        message.setFrom("cicdprojectspringemail@gmail.com");
         message.setTo(toEmail);
         message.setText(body);
         message.setSubject(subject);
--- src/main/java/ie/atu/cicdprojectemailservice/EmailServiceController.java
+++ src/main/java/ie/atu/cicdprojectemailservice/EmailServiceController.java
@@ -4,4 +4,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailServiceController {
+    private final EmailSendService emailSendService;
+
+    public EmailServiceController(EmailSendService emailSendService) {
+        this.emailSendService = emailSendService;
+    }
 }
